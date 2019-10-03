e15c3d09d4ed4b9a1d53fa2d83328e93ac09c3d1
cnrtrgt1453 <canerturgut34@gmail.com>
2019-10-03 21:38:08 +0300

    Mükemmel sayılar uygulamasını geliştirdik şimdiki uygulamamızın adı arkadaş sayılar, yani;
    "Kendileri hariç tüm pozitif bölenlerinin toplamı birbirine
             eşit olan sayılara arkadaş sayılar denir."

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /* Kendisinin dışında tüm pozitif bölenlerinin
-         sayısının toplamı kendine eşit olan sayıya mükemmel sayı denir.
-        Örneğin: 6=1+2+3, 28 */
+        /*Kendileri hariç tüm pozitif bölenlerinin toplamı birbirine
+         * eşit olan sayılara arkadaş sayılar denir.*/
 
-        Scanner input=new Scanner(System.in);
-        System.out.println("Sayi giriniz..");
-        int sayi=input.nextInt();
-        int total=0;
-        for (int i=1;i<sayi;i++)
-        {
-            if (sayi%i==0){
-                total=total+i;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1.sayıyı giriniz..");
+        int sayi1 = input.nextInt();
+
+
+        System.out.println("2. sayıyı giriniz..");
+        int sayi2 = input.nextInt();
+
+
+        int total1 = 0;
+
+        for (int i = 1; i < sayi1; i++) {
+            if (sayi1 % i == 0) {
+                total1 = total1 + i;
             }
         }
-        if (total==sayi)
-        {
-            System.out.println("MÜKEMMEL !!! ");
-        }else
-        {
-            System.out.println("Mükemmel Değil..");
+
+
+        int total2 = 0;
+        for (int i = 1; i < sayi2; i++) {
+            if (sayi2 % i == 0) {
+                total2 = total2 + i;
+            }
         }
+
+        if (total1 == total2) {
+            System.out.println("Tebrikler girmiş olduğunuz sayılar arkadaş sayılardır..");
+        } else {
+            System.out.println("Üzgünüz, girmiş olduğunuz sayılar arkadaş sayı değillerdir..:(");
+        }
+
+
     }
 }
